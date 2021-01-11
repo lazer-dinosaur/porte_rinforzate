21c707a071d999d5e003be53f8f49c30c1b29c2c
cfrancesco <testadicardi@gmail.com>
2021-01-11 21:13:43 +0100

    First commit.

---
+++ primo_tentativo.py
@@ -0,0 +1,187 @@
+import math
+
+from torch import nn
+import numpy as np
+import torch
+import tqdm
+
+
+class Model(nn.Module):
+    def __init__(self, n_doors: int):
+        super().__init__()
+        self.layers = nn.Sequential(nn.Linear(n_doors * 3, 200),
+                                    nn.ReLU(),
+                                    nn.Dropout(p=0.1),
+                                    nn.Linear(200, n_doors),
+                                    nn.Sigmoid())
+        # self.distribution = nn.Sequential(nn.Linear(1024, n_doors),
+        #                             nn.Softmax())
+        # self.predict = nn.Sequential(nn.Linear(1024, n_doors),
+        #                             nn.Sigmoid())
+    def _build_input_(self, hero_state, enemy_state, hero_rewards):
+        t = torch.tensor(np.concatenate([hero_state, enemy_state, hero_rewards]).astype(np.float32))[None, :]
+        return t
+    
+    def forward(self, x):
+        x = self.layers(x)
+        return x
+        # door = self.distribution(x)
+        # reward = self.predict(x)
+        # return door, reward
+    
+    def call(self, env_state, hero_rewards):
+        hero_state, enemy_state = env_state
+        t = self._build_input_(hero_state, enemy_state, hero_rewards)
+        x = self.forward(t)
+        # door, reward = self.forward(t)
+        
+        # return door, reward
+        return x
+        # return torch.argmax(x, dim=1) , torch.max(x, dim=1)
+
+
+class TheDoors:
+    def __init__(self, n_doors=100, decay=0.97, seed=42):
+        np.random.seed(seed)
+        self._set_initial_state(n_doors)
+        self.decay = decay
+        self.n_doors = n_doors
+    
+    def open_doors(self, choices):
+        rewards = []
+        for player_id, door in enumerate(choices):
+            state = np.random.uniform(0, 1)
+            reward = self.thresholds[door] >= state
+            rewards.append(reward.type(torch.float))
+            self.doors_states[player_id][door] += 1
+        for player_id, door in enumerate(choices):
+            self.thresholds[door] *= self.decay
+        return rewards
+    
+    def get_state(self, player_id):
+        players_state = self.doors_states[player_id]
+        other = self.doors_states[1 - player_id]
+        return players_state, other
+    
+    def reset(self):
+        self._set_initial_state(self.n_doors)
+    
+    def _set_initial_state(self, n_doors):
+        self.thresholds = torch.tensor(np.random.uniform(0, 1, n_doors))
+        self.doors_states = np.zeros((2, n_doors))
+
+
+class Player:
+    def __init__(self, player_id: int, n_doors, is_random: bool,  selection_function=None):
+        if is_random:
+            self.selection_function = lambda x,y: None
+        else:
+            self.selection_function = selection_function
+        self.player_id = player_id
+        self.n_doors = n_doors
+        self._set_initial_state(n_doors)
+        self.start_eps = 1.
+        self.end_eps = 1.e-3
+        self.eps_decay = .999
+        self.is_random = is_random
+    
+    def choose(self, environment):
+        x = self.selection_function(environment.get_state(self.player_id), self.rewards)
+        sample = np.random.uniform(0, 1)
+        eps_threshold = self.get_eps_threshold()
+
+        if sample > eps_threshold:
+            t = torch.max(x, dim=1)
+            door = t.indices
+        else:
+            door = np.random.choice(self.n_doors)
+            door = torch.tensor([[door]], dtype=torch.long)
+        return door, x
+    
+    def get_eps_threshold(self):
+        self.steps += 1
+        if self.is_random:
+            return 1.
+        else:
+            return piecewise_linear(self.steps, (0,50,200), (1., 1e-2, 1e-2))
+            # return self.end_eps + (self.start_eps - self.end_eps) * math.exp(-1. * self.steps / self.eps_decay)
+    
+    def _set_initial_state(self, n_doors):
+        self.rewards = np.zeros(n_doors)
+        self.steps = 0
+    
+    def reset(self):
+        self._set_initial_state(self.n_doors)
+
+
+def linear_function(x, x0, x1, y0, y1):
+    m = (y1 - y0) / (x1 - x0)
+    b = y0 - m * x0
+    return m * x + b
+
+
+def piecewise_linear(step, X, Y):
+    """
+    Piecewise linear function.
+
+    :param step: current step.
+    :param X: list of breakpoints
+    :param Y: list of values at breakpoints
+    :return: value of piecewise linear function with values Y_i at step X_i
+    """
+    assert len(X) == len(Y)
+    X = np.array(X)
+    if step < X[0]:
+        return Y[0]
+    idx = np.where(step >= X)[0][-1]
+    if idx == (len(Y) - 1):
+        return Y[-1]
+    else:
+        return linear_function(step, X[idx], X[idx + 1], Y[idx], Y[idx + 1])
+
+if __name__ == '__main__':
+    n_doors = 10
+    env = TheDoors(n_doors=n_doors)
+    model = Model(n_doors)
+    RL_player = Player(selection_function=model.call, player_id=0, n_doors=n_doors, is_random=False)
+    random_player = Player(player_id=1, n_doors=n_doors, is_random=True)
+    optimizer = torch.optim.AdamW(list(model.parameters()), lr=0.00001, betas=(0.999, .999), weight_decay=0.01)
+    n_games = 10000
+    wins = 0
+    diffs = []
+    diff = 0
+    iterator = tqdm.tqdm(range(n_games))
+    for _ in iterator:
+        iterator.set_description(f'{diff}')
+        env.reset()
+        RL_player.reset()
+        random_player.reset()
+        for _ in range(200):
+            optimizer.zero_grad()
+            rl_choice, rl_estimate = RL_player.choose(env)
+            random_choice, _ = random_player.choose(env)
+            rl_reward, random_reward = env.open_doors(
+                (rl_choice, random_choice))  # TODO: order depends on player_id, risks to be switched around
+            RL_player.rewards[rl_choice] += int(rl_reward)
+            random_player.rewards[random_choice] += int(random_reward)
+            log_probs = torch.log(rl_estimate)
+            supervised_loss = torch.mean(torch.abs(rl_estimate*rl_reward))
+            # supervised_loss = torch.mean(torch.abs(rl_estimate - env.thresholds))
+            # choice_loss = torch.mean(1. - rl_reward)
+            # stato, azione, stato_risultante, reward
+            # loss = torch.mean(torch.tensor([1. - float(rl_reward)]))  # ISSUE: rl_reward not a tensor
+            supervised_loss.backward()
+            # choice_loss.backward()
+            optimizer.step()
+        
+        random_player_tot_reward = np.sum(random_player.rewards)
+        RL_player_tot_reward = np.sum(RL_player.rewards)
+        diff = RL_player_tot_reward - random_player_tot_reward
+        if diff > 0:
+            wins += 1
+        diffs.append(diff)
+    print(diffs)
+    print(f'wins: {wins}')
+    
+    
+    
---
+++ train.py
@@ -0,0 +1,199 @@
+from torch import nn
+from torch import optim
+import numpy as np
+import torch
+from torch.utils.tensorboard import SummaryWriter
+
+
+class PolicyEstimator(nn.Module):
+    def __init__(self, n_inputs, n_outputs, hidden_units):
+        super().__init__()
+        self.n_inputs = n_inputs
+        self.n_outputs = n_outputs
+        self.hidden_units = hidden_units
+        
+        # # Define network
+        self.network = nn.Sequential(
+            nn.Linear(self.n_inputs, self.hidden_units),
+            nn.ReLU(),
+            nn.Linear(self.hidden_units, self.n_outputs),
+            nn.Softmax(dim=-1))
+        
+        # self.network = nn.Sequential(nn.Linear(n_inputs, 200),
+        #                              nn.ReLU(),
+        #                              # nn.Dropout(p=0.1),
+        #                              nn.Linear(200, n_outputs),
+        #                              nn.Softmax(dim=-1))
+        #                              # nn.Sigmoid())
+    
+    def forward(self, state):
+        action_probs = self.network(torch.FloatTensor(state))
+        return action_probs
+
+
+class TheDoors:
+    def __init__(self, n_doors, turns, decay=0.97, seed=42):
+        np.random.seed(seed)
+        self._set_initial_state(n_doors)
+        self.decay = decay
+        self.n_doors = n_doors
+        self.turns = turns
+    
+    def step(self, choices):
+        rewards = []
+        for player_id, door in enumerate(choices):
+            state = np.random.uniform(0, 1)
+            reward = self.thresholds[door] >= state
+            # rewards.append(reward.type(torch.float))
+            rewards.append(float(reward))
+            self.doors_states[player_id][door] += 1
+        for player_id, door in enumerate(choices):
+            self.thresholds[door] *= self.decay
+        self.steps += 1
+        done = self.steps >= self.turns
+        return rewards, done
+    
+    def get_state(self, player_id):
+        players_state = self.doors_states[player_id]
+        other = self.doors_states[1 - player_id]
+        return np.concatenate([players_state, other, self.thresholds])
+    
+    def reset(self):
+        self._set_initial_state(self.n_doors)
+    
+    def _set_initial_state(self, n_doors):
+        self.thresholds = torch.tensor(np.random.uniform(0, 1, n_doors))
+        self.doors_states = np.zeros((2, n_doors))
+        self.steps = 0
+
+
+def discount_rewards(rewards, gamma=0.99):
+    r = np.array([gamma ** i * rewards[i]
+                  for i in range(len(rewards))])
+    # Reverse the array direction for cumsum and then
+    # revert back to the original order
+    r = r[::-1].cumsum()[::-1]
+    return r - r.mean()
+
+
+def normalize(x):
+    x = (x + abs(np.min(x))) / (np.min(x) + np.max(x))
+    return x / np.sum(x)
+
+
+def reinforce(env, policy_estimator, num_episodes=2000, batch_size=10, gamma=0.99):  # Set up lists to hold results
+    total_rewards = []
+    total_rewards_random = []
+    batch_rewards = []
+    batch_actions = []
+    batch_states = []
+    tot_diffs = []
+    batch_counter = 1
+    
+    # Define optimizer
+    optimizer = optim.AdamW(policy_estimator.network.parameters(),
+                            lr=0.001)
+    # optimizer = torch.optim.AdamW(list(policy_estimator.network.parameters()), lr=0.00001, betas=(0.999, .999), weight_decay=0.01)
+    action_space = np.arange(env.n_doors)
+    ep = 0
+    global_step = 0
+    wins = 0
+    epsilon = 1.
+    while ep < num_episodes:
+        # s_0 = env.reset()
+        env.reset()
+        states = []
+        rewards = []
+        actions = []
+        rewards_random = []
+        done = False
+        while done == False:
+            global_step += 1
+            # Get actions and convert to numpy array
+            s_0 = env.get_state(player_id=0)
+            action_probs = policy_estimator.forward(
+                s_0).detach().numpy()
+            deviation = np.random.normal(0, epsilon, n_doors)
+            action_probs_dev = action_probs + deviation
+            action_probs_dev = normalize(action_probs_dev)
+            epsilon *= .99999
+            epsilon = max(1e-3, epsilon)
+            writer.add_scalar('Meta/epsilon', epsilon, global_step)
+            action = np.random.choice(action_space,
+                                      p=action_probs_dev)
+            random_action = np.random.choice(action_space)
+            # rl_choice, rl_estimate = policy_estimator.choose(env)
+            # s_1, r, done, _ = env.step(action)
+            r, done = env.step((action, random_action))
+            
+            states.append(s_0)
+            rewards.append(r[0])
+            writer.add_scalar('Reward/rl_agent', r[0], global_step)
+            writer.add_scalar('Reward/random', r[1], global_step)
+            
+            rewards_random.append(r[1])
+            actions.append(action)
+            # s_0 = s_1
+            
+            # If done, batch data
+            if done:  # fine episodio
+                batch_rewards.extend(discount_rewards(
+                    rewards, gamma))
+                batch_states.extend(states)
+                batch_actions.extend(actions)
+                batch_counter += 1
+                total_rewards.append(sum(rewards))
+                total_rewards_random.append(np.sum(rewards_random))
+                wins += int(np.sum(rewards) > np.sum(rewards_random))
+                writer.add_scalar('Wins', wins, global_step)
+                # If batch is complete, update network
+                if batch_counter == batch_size:
+                    optimizer.zero_grad()
+                    state_tensor = torch.FloatTensor(batch_states)
+                    reward_tensor = torch.FloatTensor(
+                        batch_rewards)
+                    # Actions are used as indices, must be
+                    # LongTensor
+                    action_tensor = torch.LongTensor(
+                        batch_actions)
+                    
+                    # Calculate loss
+                    logprob = torch.log(
+                        policy_estimator.forward(state_tensor))
+                    selected_logprobs = reward_tensor * torch.gather(logprob, 1, action_tensor[None, :]).squeeze()
+                    loss = -selected_logprobs.mean()
+                    writer.add_scalar('Loss', loss, global_step)
+                    
+                    # Calculate gradients
+                    loss.backward()
+                    # Apply gradients
+                    optimizer.step()
+                    
+                    batch_rewards = []
+                    batch_actions = []
+                    batch_states = []
+                    batch_counter = 1
+                
+                # avg_rewards = np.mean(total_rewards[-100:])
+                # avg_rewards_random = np.mean(total_rewards_random[-100:])
+                # Print running average
+                sort_probs = np.sort(action_probs)[::-1]
+                diff = sort_probs[:-1][:4] - sort_probs[1:][4]
+                tot_diffs.append(diff)
+                for i, d in enumerate(diff):
+                    writer.add_scalar(f'Diff/{i}', d, global_step)
+                print(f"\rEp: {ep + 1}", end="")
+                # print(f"\rEp: {ep + 1} Average of last 100: {avg_rewards} vs {avg_rewards_random} | {epsilon} | {diff}", end="")
+                ep += 1
+    
+    return total_rewards, wins, tot_diffs, total_rewards, total_rewards_random
+
+
+if __name__ == '__main__':
+    n_doors = 10
+    turns = 100
+    hidden_units = 1024
+    env = TheDoors(n_doors=n_doors, turns=turns)
+    policy_estimator = PolicyEstimator(n_inputs=3 * n_doors, n_outputs=n_doors, hidden_units=hidden_units)
+    writer = SummaryWriter(log_dir=f'logs/IU{hidden_units}')
+    out = reinforce(env, policy_estimator, num_episodes=10000, batch_size=10, gamma=0.99)
